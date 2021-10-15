e2d9be24d6ca463d4ec23b6b8c459cbcdd75788e
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2021-10-15 12:01:00 +0200

    added country project

--- src/components/projects-data/projects.jsx
+++ src/components/projects-data/projects.jsx
@@ -4,49 +4,56 @@ import Quote from '../../Image/RQG.jpg';
 import DrumMachine from '../../Image/Drum_Machine.png';
 import Pandaroma from '../../Image/pandaroma.png';
 import Profile from '../../Image/profile_portfolio.png';
+import Countries from '../../Image/Country_info_card.png';
 
 export const ProjectsData = [
-    {
-        title: 'E-commerce site',
-        discription:'Html5, Scss, ReactJs, Redux, firebase, stripe',
-        image: Commerce,
-        URL:'https://fun-clothing.herokuapp.com/',
-        id: 1
-    },
-    {
-        title: 'Monster Search',
-        discription:'Html5, Scss, ReactJs, RestAPI',
-        image:Monster,
-        URL:"https://ramu0225.github.io/Monsters-Rolodex/",
-        id: 2
-    },
-    {
-        title: 'Quote Generator',
-        discription:'Html5, Css, ReactJs, RestAPI',
-        image:Quote,
-        URL:'https://ramu0225.github.io/Random-Quote-Generator/',
-        id: 3
-    },
-    {
-        title: 'Drum Machine',
-        discription:'Html5, Css, ReactJs, RestAPI',
-        image: DrumMachine,
-        URL:'https://codepen.io/Ramu5199/pen/KKgJPyg',
-        id: 4
-
-    },
-    {
-        title: '25+5 Clock',
-        discription:'Html5, css, javaScript, ReactJs',
-        image:Pandaroma,
-        URL:'https://codepen.io/Ramu5199/pen/jOVNVvx',
-        id: 5
-    },
-    {
-        title: 'Profile Portfolio',
-        discription:'Html5, Scss, JavaScript, ReactJs',
-        image: Profile,
-        URL: 'https://ramu0225.github.io/Personal-Portfolio/',
-        id: 6
-    }
-]
+	{
+		title: "Country-Info-Card",
+		discription: "Html5, ReactJs, TypeScript, Hooks, Redux, Redux-thunk",
+		image: Countries,
+		URL: "https://countryinfo-card.netlify.app/",
+		id: 1,
+	},
+	{
+		title: "E-commerce site",
+		discription: "Html5, Scss, ReactJs, Redux, firebase, stripe",
+		image: Commerce,
+		URL: "https://fun-clothing.herokuapp.com/",
+		id: 2,
+	},
+	{
+		title: "Monster Search",
+		discription: "Html5, Scss, ReactJs, RestAPI",
+		image: Monster,
+		URL: "https://ramu0225.github.io/Monsters-Rolodex/",
+		id: 3,
+	},
+	{
+		title: "Quote Generator",
+		discription: "Html5, Css, ReactJs, RestAPI",
+		image: Quote,
+		URL: "https://ramu0225.github.io/Random-Quote-Generator/",
+		id: 4,
+	},
+	{
+		title: "Drum Machine",
+		discription: "Html5, Css, ReactJs, RestAPI",
+		image: DrumMachine,
+		URL: "https://codepen.io/Ramu5199/pen/KKgJPyg",
+		id: 5,
+	},
+	{
+		title: "25+5 Clock",
+		discription: "Html5, css, javaScript, ReactJs",
+		image: Pandaroma,
+		URL: "https://codepen.io/Ramu5199/pen/jOVNVvx",
+		id: 6,
+	},
+	{
+		title: "Profile Portfolio",
+		discription: "Html5, Scss, JavaScript, ReactJs",
+		image: Profile,
+		URL: "https://ramu0225.github.io/Personal-Portfolio/",
+		id: 7,
+	},
+];
