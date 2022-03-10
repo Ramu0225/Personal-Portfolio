d6456e6803d36ab55892b4965b0e6b4f6303623c
Ramu Akarapu <72733715+Ramu0225@users.noreply.github.com>
2022-03-10 09:57:38 +0100

    updated with projects

--- src/components/projects-data/projects.jsx
+++ src/components/projects-data/projects.jsx
@@ -5,55 +5,63 @@ import DrumMachine from '../../Image/Drum_Machine.png';
 import Pandaroma from '../../Image/pandaroma.png';
 import Profile from '../../Image/profile_portfolio.png';
 import Countries from '../../Image/Country_info_card.png';
+import Robot from '../../Image/robotintheroom.jpg';
 
 export const ProjectsData = [
+	{
+		title: "Robot-In-The-Room",
+		discription: "Html5, Sass, Model-View React framework, webpack",
+		image: Robot,
+		URL: "https://robotintheroom.netlify.app/",
+		id: 1,
+	},
 	{
 		title: "Country-Info-Card",
 		discription: "Html5, ReactJs, TypeScript, Hooks, Redux, Redux-thunk",
 		image: Countries,
 		URL: "https://countryinfo-card.netlify.app/",
-		id: 1,
+		id: 2,
 	},
 	{
 		title: "E-commerce site",
 		discription: "Html5, Scss, ReactJs, Redux, firebase, stripe",
 		image: Commerce,
 		URL: "https://fun-shopping.netlify.app/",
-		id: 2,
+		id: 3,
 	},
 	{
 		title: "Monster Search",
 		discription: "Html5, Scss, ReactJs, RestAPI",
 		image: Monster,
 		URL: "https://ramu0225.github.io/Monsters-Rolodex/",
-		id: 3,
+		id: 4,
 	},
 	{
 		title: "Quote Generator",
 		discription: "Html5, Css, ReactJs, RestAPI",
 		image: Quote,
 		URL: "https://ramu0225.github.io/Random-Quote-Generator/",
-		id: 4,
+		id: 5,
 	},
 	{
 		title: "Drum Machine",
 		discription: "Html5, Css, ReactJs, RestAPI",
 		image: DrumMachine,
 		URL: "https://codepen.io/Ramu5199/pen/KKgJPyg",
-		id: 5,
+		id: 6,
 	},
 	{
 		title: "25+5 Clock",
 		discription: "Html5, css, javaScript, ReactJs",
 		image: Pandaroma,
 		URL: "https://codepen.io/Ramu5199/pen/jOVNVvx",
-		id: 6,
+		id: 7,
 	},
 	{
 		title: "Profile Portfolio",
 		discription: "Html5, Scss, JavaScript, ReactJs",
 		image: Profile,
 		URL: "https://portfolio-ramuakarapu.netlify.app/",
-		id: 7,
+		id: 8,
 	},
 ];
